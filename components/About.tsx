5f2d2f0ae49ea4e4e76dc2028aab69cbb0b615cc
darrylwongqz92 <darryl.wong@fundnel.com>
2022-10-16 19:31:42 +0800

    finished the About section

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,13 @@ type Props = {};
 
 function About({}: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      //   viewport={{ once: true }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
@@ -13,12 +19,34 @@ function About({}: Props) {
       <motion.img
         initial={{
           x: -200,
+          opacity: 0,
         }}
-        whileInView={{ x: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="https://i.ibb.co/Sf2kvC3/pp-2.jpg"
-      ></motion.img>
-    </div>
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{' '}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{' '}
+          background
+        </h4>
+        <p className="text-sm">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </motion.div>
   );
 }
 
