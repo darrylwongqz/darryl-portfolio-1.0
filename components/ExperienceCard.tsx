c4d9e086cee93eae67c1171e1914f5daf9dfc7e2
darrylwongqz92 <darryl.wong@fundnel.com>
2022-10-16 20:36:38 +0800

    finished WorkExperience Component with Experience Card

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,82 @@
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import React from 'react';
+
+type Props = {};
+
+function ExperienceCard({}: Props) {
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        src="https://i.ibb.co/QpNpbjN/alta-logo.jpg"
+        className="w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+      />
+
+      <div className="px-0 md:px-10 max-h-96">
+        <h4 className="text-4xl font-light">Software Engineer</h4>
+        <p className="font-bold text-2xl mt-1">Alta Group</p>
+
+        <div className="flex space-x-2 my-2">
+          <Image
+            src="https://i.ibb.co/vjKjmjR/313-3133777-javascript-transparent-background-svg-hd-png-download.png"
+            alt="tech-logo"
+            height="40"
+            width="40"
+            className="rounded-full"
+          />
+        </div>
+
+        <p className="uppercase py-5 text-gray-300">
+          Started work... - Ended...
+        </p>
+
+        <ul className="list-disc space-y-4 ml-5 text-lg overflow-hidden overflow-y-scroll max-h-48 md:max-h-80">
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+          <li>
+            Summary points Summary points Summary points Summary points Summary
+            points Summary points Summary points Summary points Summary points
+            Summary points Summary points{' '}
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default ExperienceCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Head from 'next/head';
 import About from '../components/About';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Skills from '../components/Skills';
+import WorkExperience from '../components/WorkExperience';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll scroll-smooth z-0">
       <Head>
         <title>Darryl's Porfolio</title>
       </Head>
@@ -17,13 +19,17 @@ const Home: NextPage = () => {
         <Hero />
       </section>
 
-      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experience */}
 
-      {/* Skills */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
+
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* Projects */}
 
