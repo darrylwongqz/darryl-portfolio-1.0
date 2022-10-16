dffdbff6ef97fa72d2f891cb93b27207ef68cf4d
darrylwongqz92 <darryl.wong@fundnel.com>
2022-10-16 22:50:01 +0800

    added Projects.tsx and filled with dummy data

---
+++ components/Projects.tsx
@@ -0,0 +1,63 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+
+type Props = {};
+
+function Projects({}: Props) {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex relative overflow-hidden flex-col text-left md:flex-row max-w-full h-screen justify-evenly mx-auto items-center z-0"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <motion.img
+              initial={{
+                y: -300,
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              src="https://camo.githubusercontent.com/83ffddfdf9a1d874d8744be03f8293665149ef89ed024ff07ccf079f79e84b58/68747470733a2f2f692e6962622e636f2f4a64363342506b2f6e6574666c69782e706e67"
+              alt="Project-image"
+            />
+
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="decoration-[#F7AB0A]/50 underline">
+                  Case Study {i + 1} of {projects.length}:
+                </span>{' '}
+                Netflix Clone
+              </h4>
+
+              <p className="text-lg text-center md:text-left">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
+    </motion.div>
+  );
+}
+
+export default Projects;
