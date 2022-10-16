03baa38d303c657639fc09c0085480882e0d08a7
darrylwongqz92 <darryl.wong@fundnel.com>
2022-10-16 14:51:26 +0800

    whitelisted next image urls for i.ibb.co, completed animations for hero, completed hero buttons, created .heroButton component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['ibb.co', 'i.ibb.co'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
